3fcde6aee34b34b76541371532c79efb879ea7e4
Nthabiseng02 <127406242+Nthabiseng02@users.noreply.github.com>
2025-09-09 13:26:35 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var typed= new typed(".text",{
+        Strings: ["System Developer Student","Web Developer Student"], 
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+
+
+
+
+})
